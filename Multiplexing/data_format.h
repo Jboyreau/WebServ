c32e9a42ee64e846c61730242c62743bfc2f48e4
Jacques Boyreau <jacques.boyreau94@gmail.com>
2024-05-20 03:00:21 +0200

    TCP multiplexing

---
+++ Multiplexing/data_format.h
@@ -0,0 +1,13 @@
+#ifndef DATA_FORMAT_H
+#define DATA_FORMAT_H
+typedef struct operand_s
+{
+	unsigned int a;
+	unsigned int b;
+} operand_t;
+
+typedef struct result_s
+{
+	unsigned int c;
+} result_t;
+#endif
